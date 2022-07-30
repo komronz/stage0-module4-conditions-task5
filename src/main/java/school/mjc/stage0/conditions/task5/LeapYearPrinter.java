09136199abd0d2b0c187b6a290d3a0587319b843
komronz <pandacoder0820@gmail.com>
2022-07-30 16:10:15 +0500

    module4Task5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -5,14 +5,10 @@ public class LeapYearPrinter {
         boolean isLeap = false;
         if (year % 4 == 0) {
             if (year % 100 == 0) {
-                if (year % 400 == 0)
-                    isLeap = true;
-                else
-                    isLeap = false;
+                isLeap = year % 400 == 0;
             } else
                 isLeap = true;
-        } else
-            isLeap = false;
+        }
         System.out.println(isLeap ?"leap":"not leap");
     }
 }
